d7d45b9c2b215090930de0e6af70cad34b979f20
MrTrnr <marietournier.pro@gmail.com>
2022-02-09 12:24:09 +0100

    ajout composant avis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AvisComponent } from './avis/avis.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AvisComponent
   ],
   imports: [
     BrowserModule
