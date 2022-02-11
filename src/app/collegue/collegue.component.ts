01162f21276ec5d4a14ea6a2e6255b49ecc8f150
MrTrnr <marietournier.pro@gmail.com>
2022-02-11 07:47:35 +0100

    traitement boutons actifs si score < ou > -1000/1000

--- src/app/collegue/collegue.component.ts
+++ src/app/collegue/collegue.component.ts
@@ -10,21 +10,39 @@ export class CollegueComponent implements OnInit {
 
   // @Input() col!: Collegue;
 
-
-
   col: Collegue = {
     pseudo: 'moi super pseudo',
     photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/6/6c/ValeYellow46_Signature.png',
     score: 0
-  };
-  // pseudo!: string;
-  // adresseUrl!: string;
-  // score!: number;
 
+
+  };
+  
+  boutonJaimeActif = false;
+  boutonJeDetesteActif = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.gererEtatBoutons();
+    }
+
+    gererEtatBoutons(){
+      if(this.col){
+        const{score}=this.col;
+        if(score >= 1000){
+          this.boutonJaimeActif = false;
+          this.boutonJeDetesteActif = true;
+        }
+        else if (score <= -1000){
+          this.boutonJeDetesteActif = false;
+          this.boutonJaimeActif = true;
+        }
+        else {
+          this.boutonJaimeActif=true;
+          this.boutonJeDetesteActif=true;
+      }
+      }
     }
 
   traiterEvtAvis(avisEmis: Avis){
@@ -36,6 +54,7 @@ export class CollegueComponent implements OnInit {
         this.col.score = this.col.score - 100;
       }
     }
+    this.gererEtatBoutons();
   }
 
 }
