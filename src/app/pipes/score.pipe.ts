99b18594ef17bf5741cdd32eab18cfb773cb9836
MrTrnr <marietournier.pro@gmail.com>
2022-02-11 09:36:10 +0100

    ajout pipe score

--- src/app/pipes/score.pipe.ts
+++ src/app/pipes/score.pipe.ts
@@ -1,12 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
-
+import { Collegue } from '../model';
 @Pipe({
   name: 'score'
 })
 export class ScorePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(score:number): string {
+    if (score < 0){
+      return '- ' + (-score);
+    }else if(score > 0){
+      return '+ ' + score;
+    }
+    else {return '0'}
   }
 
 }
