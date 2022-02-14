2eb9dd00152f18c46285d87b2cb6ea971435b104
MrTrnr <marietournier.pro@gmail.com>
2022-02-14 14:54:38 +0100

    ajout page detail avec routing

--- src/app/detail-collegue/detail-collegue.component.ts
+++ src/app/detail-collegue/detail-collegue.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { map, Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-detail-collegue',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailCollegueComponent implements OnInit {
 
-  constructor() { }
+  id: Observable<string | null>;
+
+  msg = '';
+
+  constructor(private aRoute: ActivatedRoute, private router: Router) {
+
+    this.id =  this.aRoute.paramMap.pipe(
+      map(paramMap => paramMap.get('id'))
+    );
+   }
 
   ngOnInit(): void {
+
+
+
+  }
+
+  modifier() {
+    this.msg = 'modification en cours';
+    setTimeout(() => this.router.navigateByUrl('/liste'), 2000);
   }
 
 }
